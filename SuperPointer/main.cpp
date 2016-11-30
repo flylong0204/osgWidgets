591a45f930c947a1feb300a939dea090705fcaca
马飞 <hellomafei@163.com>
2016-12-01 01:16:38 +0800

    添加了注释

--- SuperPointer/main.cpp
+++ SuperPointer/main.cpp
@@ -4,6 +4,11 @@
 
 int main()
 {
+    /*
+     * 这里需要注意，超级指针的对象不需要加*
+     * 但是调用该对象的方法的时候要用->符号
+     *
+     */
     osg::ref_ptr<osgViewer::Viewer> viewer = new osgViewer::Viewer;
     osg::ref_ptr<osg::Node> node = new osg::Node;
     node = osgDB::readNodeFile("../data/model/avatar.osg");
