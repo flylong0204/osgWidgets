f022fbeed30ab1253c55f2cb87e39004edd08c2a
马飞 <hellomafei@163.com>
2016-12-01 17:26:15 +0800

    更换模型

--- osgBasic/main.cpp
+++ osgBasic/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     osgViewer::Viewer viewer;
     osg::Node *node = new osg::Node;
-    node = osgDB::readNodeFile("../data/model/avatar.osg");
+    node = osgDB::readNodeFile("../data/model/text.DAE");
     viewer.setSceneData(node);
     return viewer.run();
 }
